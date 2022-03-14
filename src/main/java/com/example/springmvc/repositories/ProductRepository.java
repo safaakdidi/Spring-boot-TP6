c6d1819f4f5187f13f6844c6e5f88f5a4972fabf
safaakedidi <safakedidi30@gmail.com>
2022-03-15 00:05:09 +0100

    first commit

---
+++ src/main/java/com/example/springmvc/repositories/ProductRepository.java
@@ -0,0 +1,16 @@
+package com.example.springmvc.repositories;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.stereotype.Repository;
+
+import com.example.springmvc.models.Product;
+
+@Repository
+public interface ProductRepository  extends JpaRepository<Product,Long> {
+	
+	
+	Product findByProdName(final String prodName);
+	
+}
